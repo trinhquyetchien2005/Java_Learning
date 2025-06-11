8df8a97606ceba86ffcbe9aadf7105127cdc24b3
TrinhQuyetChien <trinhquyetchien2005@gmail.com>
2025-06-11 13:28:27 +0700

    chore(java): chane layout and fix lsp java

---
+++ src/Tutorials/Core.java
@@ -0,0 +1,11 @@
+package src.Tutorials;
+
+import java.util.Scanner;
+
+public class Core{
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        String name = sn.toString();
+        System.out.println(name); 
+    }
+}
